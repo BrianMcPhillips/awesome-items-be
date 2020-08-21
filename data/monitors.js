68dc5633fdf958fe17b06e1a0c1d5b3bfa7963ff
Brian McPhillips <B.McPhillips89@gmail.com>
2020-08-20 22:30:58 -0700

    finally done

--- data/monitors.js
+++ data/monitors.js
@@ -6,7 +6,7 @@ module.exports = [
     is_sick: false,
     model: '8010A',
     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSXvdxydVBGi8bDW0dDSMxDEfiaYXF49Au1SG4kfNmYTdbayh3NRcJnisqicjctk3bdj0W9qdg&usqp=CAc',
-    brands_id: 2,
+    brands_id: 1,
   },
   {
     cool_factor: 8,
@@ -14,7 +14,7 @@ module.exports = [
     is_sick: true,
     model: '8020D',
     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdMkeQDWLC2_kfK4kf3M6K3fZDDAFauMaloTRPK1NDnPXTcbEcYgXJq5Xudnu84lHY6y2z1SA&usqp=CAc',
-    brands_id: 2,
+    brands_id: 1,
   },
   {
     cool_factor: 10,
@@ -22,7 +22,7 @@ module.exports = [
     is_sick: true,
     model: '8040B',
     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSqrOdTrg7ARO86E__AheIK0JBmYjWwg52f-s7evrAwPi9LBXI0ix-GnlIlCHVM7FVrpFayzUkX&usqp=CAc',
-    brands_id: 2,
+    brands_id: 1,
   },
   {
     cool_factor: 9,
@@ -30,7 +30,7 @@ module.exports = [
     is_sick: true,
     model: 'A7X',
     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQkYa4PhQG4xHoDsTnp_b3JbPmzu1nUPzsgLwDsZG-XZTSkSl6cNn2GrUdeaV3IvbFAuKwRobC3&usqp=CAc',
-    brands_id: 3,
+    brands_id: 2,
   },
   {
     cool_factor: 4,
@@ -38,7 +38,7 @@ module.exports = [
     is_sick: false,
     model: 'T8V',
     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMWuooM4tiltjbA9946IEAU9rMqcOBlwH93z6-hDflnngiK-OvSRhBJ7jK8vMKeBBdnqzOyJk8&usqp=CAc',
-    brands_id: 3,
+    brands_id: 2,
   },
   {
     cool_factor: 6,
@@ -46,7 +46,7 @@ module.exports = [
     is_sick: true,
     model: 'A3X',
     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTynE5wyyO3mSRCEBk8Aa-HaSBNNrdNasrTWkYtcvc49pQBYSZwXSw8npSNfLE&usqp=CAc',
-    brands_id: 3,
+    brands_id: 2,
   },
 
 ];
